89908e2e10d5bf4091b9c0030122dbe2c07a7cb7
DonatasNoreika <donoras@gmail.com>
2024-01-10 08:10:44 +0200

    +Padaryti, kad užsakymo formoje būtų matomos ir užsakymo eilutės (į jas galima būtų įrašyti informaciją)

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from .models import Service, VehicleModel, Vehicle, Order, OrderLine
 
+
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['date', 'vehicle']
+    inlines = [OrderLineInLine]
 
-# Register your models here.
+
 admin.site.register(Service)
 admin.site.register(Vehicle)
 admin.site.register(VehicleModel)
