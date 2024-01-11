fed58f93d393ebdd4b66c5f7885b78babb6f8246
DonatasNoreika <donoras@gmail.com>
2024-01-11 09:02:50 +0200

    +Sukurti puslapį (ne admin), kuriame būtų matoma statistika: paslaugų kiekis, atliktų užsakymų kiekis, automobilių kiekis

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Service, Order, Vehicle
+
 
 # Create your views here.
+def index(request):
+    num_services = Service.objects.all().count()
+    num_orders_done = Order.objects.filter(status__exact='i').count()
+    num_vehicles = Vehicle.objects.all().count()
+    result = {
+        "num_services": num_services,
+        "num_orders_done": num_orders_done,
+        "num_vehicles": num_vehicles,
+    }
+    return render(request, template_name="index.html", context=result)
